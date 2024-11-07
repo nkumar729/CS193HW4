f991394ed425d723e359c4eb8daf3c810a3cbf1a
Nila U Kumar <kumar729@data.cs.purdue.edu>
2024-11-07 16:08:52 -0500

    Changed switch statement to use if-else statement

--- src/Test1.java
+++ src/Test1.java
@@ -9,26 +9,21 @@ public class Test1 {
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
 
-        switch(operation) {
-            case ADD:
-                // if operation = ADD, return the sum
-                result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
-                result = num1 * num2;
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
-                    System.out.println("Can't divide by 0");
-                } else {
-                    result = ((double)num1) / num2;
-                }
-                break;
-        }
-        return result;
+        if (operation == Operation.ADD) {
+	 result = num1 + num2;
+	} else if (operation == Operation.SUBTRACT) {
+	 result = num1 - num2;
+	} else if (operation == Operation.MULTIPLY) {
+	 result = num1 * num2;
+	} else if (operation == Operation.DIVIDE) {
+	 if (num2 == 0) {
+	  System.out.println("Can't divide by 0");
+	 } else {
+	  result = ((double)num1) / num2;
+	 }
+	}
+
+	return result;
     }
 
     public static void main(final String[] args) {
